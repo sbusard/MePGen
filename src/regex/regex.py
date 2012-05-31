75c48de1adf51fa2e26eafd6fb4f2ac0b8edac20
Simon Busard <simon.busard@uclouvain.be>
2012-05-31 07:57:48 +0200

    Implementing regex. In progress.

---
+++ src/regex/regex.py
@@ -0,0 +1,22 @@
+class Regex():
+	"""
+	A Regex represents a regular expression. Each Regex has a type
+	(CONCAT, CHOICE, REPEAT, RANGE).
+	
+	For example, the regular expression (a.(b|c))* is represented by
+	(	REPEAT, 
+		(	CONCAT,
+			(RANGE('a')),
+			(	CHOICE,
+				(RANGE('b')),
+				(RANGE('c'))
+			)
+		)
+	)
+	"""
+	
+	def __init__(self, type, left = None, right = None, range = None):
+		self.type = type
+		self.left = left
+		self.right = right
+		self.range = range
---
+++ src/regex/transformation.py
@@ -0,0 +1,5 @@
+def regex_to_automaton(regex):
+	"""
+	Transforms regex into a non-deterministic automaton.
+	"""
+	# TODO
