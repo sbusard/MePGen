8b7df70134e6e129c4a3254a2678b0ac81c5969d
Simon Busard <simon.busard@uclouvain.be>
2012-05-31 10:46:35 +0200

    Implementing regex has a classes hierarchy. Regex can now be built by instantiating the right classes.

--- src/regex/regex.py
+++ src/regex/regex.py
@@ -18,11 +18,47 @@ class Regex():
 	# Regex types
 	CONCAT, CHOICE, REPEAT, RANGE = range(4)
 	
-	def __init__(self, type, left = None, right = None, range = None):
-		self.type = type
+	def __init__(self, regtype):
+		self.type = regtype
+		
+
+class Concat(Regex):
+	
+	def __init__(self, left, right):
+		super().__init__(Regex.CONCAT)
 		self.left = left
 		self.right = right
-		self.range = range
 		
 	def __str__(self):
-		# TODO
+		return "(" + str(self.left) + " . " + str(self.right) + ")"
+		
+
+class Choice(Regex):
+
+	def __init__(self, left, right):
+		super().__init__(Regex.CHOICE)
+		self.left = left
+		self.right = right
+		
+	def __str__(self):
+		return "(" + str(self.left) + " | " + str(self.right) + ")"
+		
+		
+class Repeat(Regex):
+	
+	def __init__(self, child):
+		super().__init__(Regex.REPEAT)
+		self.child = child
+	
+	def __str__(self):
+		return "(" + str(self.child) + ")*"
+
+
+class Range(Regex):
+	
+	def __init__(self, regrange):
+		super().__init__(Regex.RANGE)
+		self.range = regrange
+		
+	def __str__(self):
+		return "[" + str(self.range) + "]"
