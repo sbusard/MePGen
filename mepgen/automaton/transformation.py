794cce89745de67b641de966560a43e9e01c3c60
Simon Busard <simon.busard@uclouvain.be>
2012-06-02 22:33:38 +0200

    Implementing lambda transitions removal.

--- mepgen/automaton/transformation.py
+++ mepgen/automaton/transformation.py
@@ -1,3 +1,5 @@
+from .state import LAMBDA
+
 def minimize(automaton):
 	"""
 	Returns the minimization of automaton.
@@ -17,7 +19,28 @@ def remove_lambdas(automaton):
 	Returns a new automaton accepting the same language as automaton but
 	containing no lambda transition.
 	"""
-	pass # TODO
+	
+	def _get_lambda_reachable(state):
+		"""
+		Returns the set of states reachable
+		through lamda transitions from state.
+		"""
+		
+		pending = [state]
+		visited = set()
+		reachable = set()
+		
+		while len(pending) > 0:
+			s0 = pending.pop()
+			visited.add(s0)
+			if LAMBDA in s0.successors:
+				reachable = reachable | s0.successors[LAMBDA]
+				for s in [s for s in s0.successors[LAMBDA] \
+							if s not in (visited | set(pending))]:
+					pending.append(s)
+				
+		return reachable
+			
 	
 	# Copy the automaton, then
 	# for each state (use a stack and a visited set to compute all states once)
@@ -31,3 +54,42 @@ def remove_lambdas(automaton):
 	# Note: we can remove lambda transitions from the current state and do not
 	# wait until the end of the visit since every one-non-lambda-character paths
 	# are preserved by the addition of new transitions.
+	
+	automaton = automaton.copy()
+	
+	pending = [automaton.initial]
+	visited = set()
+	
+	while len(pending) > 0:
+		s0 = pending.pop()
+		visited.add(s0)
+		
+		reachable = _get_lambda_reachable(s0)
+		
+		for s in reachable:
+			for c in [c for c in s.successors if c != LAMBDA]:
+				for sp in s.successors[c]:
+					s0.add_successor(c, sp)
+			if s in automaton.accepting:
+				automaton.accepting.add(s0)
+				
+		for c in s0.successors:
+			for s in s0.successors[c]:
+				if s not in visited | set(pending):
+					pending.append(s)
+				
+	pending = [automaton.initial]
+	visited = set()
+	while len(pending) > 0:
+		s0 = pending.pop()
+		visited.add(s0)
+		
+		for c in s0.successors:
+			for s in [s for s in s0.successors[c] if s not in visited]:
+				pending.append(s)
+				
+		if LAMBDA in s0.successors:
+			del s0.successors[LAMBDA]
+		
+	return automaton
+		
