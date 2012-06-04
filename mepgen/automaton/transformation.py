ad3be3f41ea71e86d0a96fdc0a89d8e80af207a1
Simon Busard <simon.busard@uclouvain.be>
2012-06-04 12:38:10 +0200

    Implementing determinisation algorithm.

--- mepgen/automaton/transformation.py
+++ mepgen/automaton/transformation.py
@@ -1,4 +1,5 @@
-from .state import LAMBDA
+from .state import LAMBDA, State
+from .automaton import Automaton
 
 def minimize(automaton):
 	"""
@@ -11,7 +12,67 @@ def determinize(automaton):
 	"""
 	Returns the determinization of automaton.
 	"""
-	pass # TODO
+	
+	def _get_chars(stateset):
+		"""
+		Returns the set of characters such that, for each character c,
+		there is at least one state in stateset having a transition labeled
+		with c.
+		"""
+		
+		chars = set()
+		for s in stateset:
+			chars = chars | set(s.successors)
+			
+		return chars
+		
+	
+	# Subsets of states of automaton become states of the determinized automaton
+	# Start with the subset of initial states
+	# For each new state s, there is an x-transition to another state s' iff
+	# there is a state in s with an x-transition to s';
+	
+	# Use a stack and a dictionary; the stack stores the new states that have
+	# no transitions yet, the ditionary stores the information about which
+	# subset of states correspond to which new state.
+	# - Start with the stack filled with the subset containing only the initial
+	# state, the dictionary contains the mapping between this subset and the new
+	# state. Be aware of acceptance.
+	# - While the stack in non-empty,
+	#		- take the next subset
+	#		- add transitions for this subset
+	#			- for each character,
+	#				- get the union of the successors
+	#					of the states of the subset
+	#				- if this new subset has no corresponding state,
+	#					- create it and add the subset to the stack
+	#					- add it to accepting ones if one of them is accepting
+	#				- add a transition with this character from the new
+	#					state of the subset to the corresponding state
+	
+	ss0 = frozenset([automaton.initial])
+	pending = [ss0]
+	mapping = {ss0:State()}
+	accepting = set()
+	if automaton.initial in automaton.accepting:
+		accepting.add(mapping[ss0])
+	
+	while len(pending) > 0:
+		ss = pending.pop()
+		cc = _get_chars(ss)
+		for c in cc:
+			succs = set()
+			for s in ss:
+				succs = succs | s.successors[c]
+			succs = frozenset(succs)
+			if succs not in mapping:
+				mapping[succs] = State()
+				pending.append(succs)
+				if len(succs & automaton.accepting) > 0:
+					accepting.add(mapping[succs])
+			mapping[ss].add_successor(c, mapping[succs])
+			
+	return Automaton(mapping[ss0], accepting)
 	
 	
 def remove_lambdas(automaton):
